f14a691bffce6efa4047fab731eb284f0db99034
Pavel Khorenyan <pavelkhorenyan@gmail.com>
2025-12-28 21:12:50 +0300

    lualine

--- nvim/init.lua
+++ nvim/init.lua
@@ -37,3 +37,53 @@ vim.opt.wildmenu = true
 
 -- Показывать незавершенные команды в статусной строке
 vim.opt.showcmd = true
+
+
+vim.cmd[[
+  augroup LualineAutoLoad
+    autocmd!
+    autocmd VimEnter * lua load_lualine()
+  augroup END
+]]
+
+function load_lualine()
+  -- Add plugin paths to package path
+  local plugin_path = vim.fn.expand('~/.config/nvim/lua/plugins/')
+
+  
+  -- Load nvim-web-devicons
+  package.path = package.path .. ';' .. plugin_path .. 'nvim-web-devicons/?.lua'
+  package.path = package.path .. ';' .. plugin_path .. 'nvim-web-devicons/lua/?.lua'
+  require('nvim-web-devicons').setup()
+  
+  -- Load lualine
+  package.path = package.path .. ';' .. plugin_path .. 'lualine/?.lua'
+  package.path = package.path .. ';' .. plugin_path .. 'lualine/lua/?.lua'
+  
+
+  require('lualine').setup({
+    options = {
+      theme = 'auto',
+      icons_enabled = true,
+      component_separators = '|',
+      section_separators = '',
+    },
+
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_c = {'filename'},
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'filename'},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  })
+end
